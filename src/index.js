989f07961d52bced984359ae68541e8196c698a6
594521765 <594521765@qq.com>
2017-12-13 18:06:52 +0800

    Revert "index 页面修改"
    This reverts commit 28a2df4d6aeda0dec921e40204f321b7189b8900.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import { hashRouter } from 'dva/router';
 import createLoading from 'dva-loading';
 import { message } from 'antd';
 import routerConfig from './routerConfig';
+// import createLogger from 'redux-logger';
+// import storeModel from './storeModel';
 
 const onError = (e) => { // 整体的错误处理
   const { msg } = e;
   console.log(msg);
   message.error(msg);
+  // if (msg === 'request_error') {
+  //   console.log('请求超时，请重新登录！');
+  // } else {
+  //   console.log('123456789');
+  // }
 };
 
 const app = dva({
